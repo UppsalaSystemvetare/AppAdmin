fc88fb973529e765e55a5a583a228bd4610c6119
HannesSam <hannessamskog@gmail.com>
2019-11-21 17:14:15 +0100

    Update users.php

--- users.php
+++ users.php
@@ -1,15 +1,16 @@
 <?php
 include("include/models/header.php");
 include("include/models/users_model.php");
-
-$connection = connect();
-$all = "SELECT * FROM Users";
-$result = $connection->query($all);
-$connection = disconnect();
 ?>
 
-<?php while ($row = $result->fetch_assoc()) { ?>
+<?php 
+$result = Users::get_users();
+while ($row = $result->fetch_assoc()) { ?>
     <tr>
         <td><?php echo $row["ID"] ?></td>
+        <td> Name</td>
+        <td><?php echo $row["Rank"] ?></td>
+        <td><?php echo $row["Team"] ?></td>
+        <br>
     </tr>
 <?php } ?>
