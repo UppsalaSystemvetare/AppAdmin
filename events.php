800efebec7db9ec3991746b8c67ac32058ecabe2
kvacke <jens.bleckert@gmail.com>
2019-11-21 16:48:53 +0100

    Lagt till formulär och lite kommentarer för fortsatt arbete

--- events.php
+++ events.php
@@ -7,19 +7,51 @@ $all = "SELECT * FROM Events";
 $result = $connection->query($all);
 $connection = disconnect();
 //Hello Hannes här!
+
+//BACKEND:
+// Lägga till formulärdatan i databasen
+// Radera en rad i databasen givet ett ID
+// Ändra en rad i databasen givet ett ID
+// Hämta id och namn på alla faddrar från tabellen "Faddrar"
+
+//FRONTEND:
+// Stil
+// Knappar för att ändra och ta bort
+// Rullista för nykterfaddrar
+// Tabellen ska presentera namn på nykterfaddrar och inte ID?
+
+
+
+
 ?>
 
 <html lang="en">
     <head>
         <title>Admin Inspark</title>
-        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html" charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div id="conatiner">
+        <div id="form-container">
+            <form id="add-event" action="event_actions.php">
+                <table>
+                <tr><td>Titel: </td><td><input type="text" id="title"></td></tr>
+                <tr><td>Beskrivning: </td><td><input type="text" id="description"></td></tr>
+                <tr><td>Datum: </td><td><input type="date" id="datetime"></td></tr>
+                <tr><td>Tid: </td><td><input type="time" id="starttime"></td></tr>
+                <tr><td>Plats: </td><td><input type="text" id="location"></td></tr>
+                <tr><td>Nykterfadder 1: </td><td><input type="text" id="patron1"></td></tr>
+                <tr><td>Nykterfadder 2: </td><td><input type="text" id="patron2"></td></tr>
+                </table>
+                <input type="submit" value="Submit">
+            </form>
+
+        </div>
+
+        <div id="table-container">
             <table class="table">
                 <thead>
                 <tr>
@@ -52,3 +84,10 @@ $connection = disconnect();
         </div>
     </body>
 </html>
+
+
+<?php
+
+//echo "<div id= $row['id']>" . "<$row['id']" .  
+
+?>
