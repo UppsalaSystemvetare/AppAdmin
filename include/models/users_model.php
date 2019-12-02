12825b3f77344b21b2ee389fccccf0be8ee445ed
Christoffer Wrigmar <50480487+wrigmarism@users.noreply.github.com>
2019-12-02 10:39:00 +0100

    Update users_model.php
    Ändrat från refresh till return + lagt till funktion för att ta bort och söka efter användare.

--- include/models/users_model.php
+++ include/models/users_model.php
@@ -12,10 +12,27 @@ class Users{
     static public function change_team($id, $rank){
       $connection = connect();
       $query = "UPDATE Users SET Rank = '$rank' WHERE ID = '$id'";
-      if(!$result = $connection->query($result)){
-        echo("Query error: " . $result->query_error);
-        header("Location:../users.php");
-      }
+      $result = $connection->query($result);
+      $connection = disconnect();
+      return $result;
+
+    //Vill vi kunna ta bort användare utifrån fler egenskaper än id, ex namn? C.
+    static public function delete_user($id){
+      $connection = connect();
+      $query = "DELETE FROM Users WHERE ID = '$id'";
+      $result = $connection->query($result);
+      $connection = disconnect();
+      return $result;
+
+    static public function search($value){
+      $connection = connect();
+      $query = "SELECT * FROM Users WHERE ID = '$value' OR
+                                          User_ID = '$value' OR
+                                          Rank = '$value' OR
+                                          Team = '$value'";
+      $result = $connection->query($result);
+      $connection = disconnect();
+      return $result;
     }
   }
  ?>
