bf970ebc1f6b3e876de57bab044e5a1b45daf6ed
Vildmusen <viktorvilmusenaho@hotmail.com>
2020-01-12 15:03:57 +0100

    samma meny överallt, flyttade runt lite filer

---
+++ include/html/menu.php
@@ -0,0 +1,30 @@
+<html lang="en">
+
+<head>
+    <title>Admin Inspark</title>
+    <meta charset="utf-8">
+    <link href="https://uppsalasystemvetare.se/wp-content/themes/uppsalasystemvetare/img/favicon.png" rel="shortcut icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/users.css">
+    <script src="https://kit.fontawesome.com/9d81ab243e.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-blue">
+        <a class="navbar-brand" href="index.php">
+            <img src="https://uppsalasystemvetare.se/wp-content/themes/uppsalasystemvetare/img/favicon.png" width="30" height="30" alt=""></a>
+
+        <div class="navbar-nav">
+            <a id="home-menu" class="nav-item nav-link" href="index.php">Home</a>
+            <a id="event-menu" class="nav-item nav-link" href="events.php">Events</a>
+            <a id="users-menu" class="nav-item nav-link" href="users.php">Users</a>
+            <a id="missions-menu" class="nav-item nav-link" href="missions.php">Missions</a>
+            <a id="weekmissions-menu" class="nav-item nav-link" href="weekmissions.php">Week-Missions</a>
+            <a class="nav-item nav-link" href="include/models/logout_process.php">Log Out</a>
+        </div>
+    </nav>
