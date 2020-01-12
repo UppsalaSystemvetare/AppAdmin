084888b2ca6ae9451294df6f5f22f975d7368bc9
Vildmusen <viktorvilmusenaho@hotmail.com>
2020-01-12 15:47:04 +0100

    weekmissions kopierad från missions, tpy

--- weekmissions.php
+++ weekmissions.php
@@ -1,70 +1,70 @@
+
 <?php
 include("include/models/weekmissions_model.php");
 include("include/html/menu.php");
 ?>
 
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button class="btn btn-secondary" type="button">Modify Mission</button>
+        <button class="btn btn-secondary" type="button" onclick="scrollToCreate()">Add New Missions</button>
+        <button class="btn btn-danger" type="button" id="delete">Delete <i class="fas fa-trash-alt"></i></button>
+    </div>
+        
+    <table class="table table-striped table-bordered table-sm sortable" id="all_missions">
+        <thead id="table-header">
+            <tr>
+                <th>Select</th>
+                <th>Id</th>
+                <th>Description</th>
+                <th>Point value (0 if user-controlled)</th>
+                <!-- <th>Ändra</th>
+                <th>Ta bort</th> -->
+            </tr>
+        </thead>
 
-        <div id="container">
-            <div class="header">
-                <div class="section-button selected" id="all_missions_button">
-                    <h1 onclick="change_tab('table')">All Missions</h1>
+        <tbody id="all_missions_body">
+
+        <?php 
+            $result = WeekMissions::get_missions();
+            while($row = $result->fetch_assoc()) { 
+        ?>
+            <tr>
+                <td><input type="checkbox" aria-label="Checkbox for following text input"></td>
+                <td><?php echo $row["Id"]?></td>
+                <td><?php echo $row["Description"]?></td>
+                <td><?php echo $row["PointValue"]?></td>
+                <!-- <td><button class="btn-xs btn-secondary" id="<?php echo $row['Id'] ?>" onclick="change_mission(<?php echo $row['Id'] ?>)">Ändra</button></td>
+                <td><button class="btn-xs btn-danger" id="<?php echo $row['Id'] ?>" onclick="delete_mission(<?php echo $row['Id'] ?>)">Ta bort</button></td> -->
+                <input class="Id" type="hidden" value="<?php echo $row["Id"] ?>">
+            </tr>
+        <?php }?>
+        
+        </tbody>
+    </table>
+             
+    <div class="content" id="create-missions">
+        <h2>Create single new mission:</h2>
+            <form action="createMissionsDB.php" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="desc">Description</label>
+                    <input id="desc" name="DESC" type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Description of the mission">
                 </div>
-                <div class="section-button" id="create_missions_button">
-                    <h1 onclick="change_tab('create')">Create Missions</h1>
-                </div>
-            </div>        
-            <div class="content">
-                <table class="table" id="all_missions">
-                    <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Beskrivning</th>
-                        <th>Poängvärde (0 om användaren själv ska skriva in)</th>
-                        <th>Ändra</th>
-                        <th>Ta bort</th>
-                        <th>Markera</th>
-                    </tr>
-                    </thead>
-                    <tbody id="all_missions_body">
-                    <?php 
-                        $result = WeekMissions::get_missions();
-                        while($row = $result->fetch_assoc()) { 
-                    ?>
-                        <tr>
-                            <td><?php echo $row["Id"]?></td>
-                            <td><?php echo $row["Description"]?></td>
-                            <td><?php echo $row["PointValue"]?></td>
-                            <td><button class="btn btn-secondary" id="<?php echo $row['Id'] ?>" onclick="change_mission(<?php echo $row['Id'] ?>)">Ändra</button></td>
-                            <td><button class="btn btn-danger" id="<?php echo $row['Id'] ?>" onclick="delete_mission(<?php echo $row['Id'] ?>)">Ta bort</button></td>
-                            <td><input type="checkbox"></td>
-                            <input class="Id" type="hidden" value="<?php echo $row["Id"] ?>">
-                        </tr>
-                       
-                    <?php }?>
-                    <button class="btn btn-primary" type="button" id="delete" >Ta bort markerade</button>
-                    </tbody>
-                </table>
-                <div id="create_missions" class="table hidden">
-                    <h2>Create new mission:</h2>
-                    <form action="createMissionsDB.php" method="post" enctype="multipart/form-data">
-                        <div class="input-group input-group-lg">
-                            <input name="DESC" type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Beskrivning...">
-                            <input name="POINTS" type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Poängvärde...">
-                            <input class="btn btn-primary" type="submit" value="Submit">
-                        </div> 
-                    </form>
-                </div>
-                <div id="create_multiple_missions" class="table_hidden">
-                    <h2>Create multiple new mission:</h2>
-                    <form action="createMissionsDB.php" method="post" enctype="multipart/form-data">
-                        <div class="input-group input-group-lg">
-                            <input name="FILE" type="file">
-                            <input class="btn btn-primary" type="submit" value="Submit">
-                        </div> 
-                    </form>
-                </div>
-            </div>
-        </div>
-    </body>
-    <script src="assets/js/weekmission.js"></script>
+                <div class="form-group">
+                    <label for="point">Point Value</label>
+                    <input id="point" name="POINTS" type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Poängvärde...">
+                </div> 
+                <input class="btn btn-primary" type="submit" value="Submit">
+            </form>
+        <h1> - OR - </h1>
+        <h2>Create multiple new missions: (.xls, .xlsx)</h2>
+            <form action="createMissionsDB.php" method="post" enctype="multipart/form-data">
+                <div class="form-group input-group-lg">
+                    <input name="FILE" type="file">
+                </div> 
+                <input class="btn btn-primary" type="submit" value="Submit">
+            </form>
+            </div>     
+    </div>
+</body>
+<script src="assets/js/weekmission.js"></script>
 </html>
